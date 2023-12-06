c1a3b0640cc36cb1a990df2da83bff25dd85d7b6
ThinLiquid <thnlqd@gmail.com>
2023-12-06 01:16:59 +0000

    Create spotify.js

---
+++ apps/spotify.js
@@ -0,0 +1,56 @@
+export default class SpotifyApp {
+  meta = {
+    name: 'Spotify',
+    description: 'Spotify is a digital music service that gives you access to millions of songs.',
+    icon: 'https://raw.githubusercontent.com/Flow-Works/AppRepo/main/assets/spotify.svg',
+    pkg: 'spotify.spotify',
+    version: '1.0.0'
+  }
+
+  async open () {
+    const win = window.wm.createWindow({
+      title: this.meta.name,
+      icon: this.meta.icon,
+      width: 700,
+      height: 500
+    })
+
+    const xor = {
+      randomMax: 100,
+      randomMin: -100,
+
+      encode: (str) => {
+        if (!str) return str
+        return encodeURIComponent(
+          str
+            .toString()
+            .split('')
+            .map((char, ind) =>
+              ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
+            )
+            .join('')
+        )
+      },
+      decode: (str) => {
+        if (!str) return str
+        const [input, ...search] = str.split('?')
+
+        return (
+          decodeURIComponent(input)
+            .split('')
+            .map((char, ind) =>
+              ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
+            )
+            .join('') + (search.length ? '?' + search.join('?') : '')
+        )
+      }
+    }
+
+    win.content.style.background = 'var(--base)'
+    win.content.innerHTML = `
+      <iframe src="/service/${xor.encode('https://open.spotify.com')}" style="width: 100%;height: 100%;border: none;"/>
+    `
+
+    return win
+  }
+}
