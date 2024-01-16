1c8ad9caea8797004741a761fda772804da9eab2
ThinLiquid <thnlqd@gmail.com>
2024-01-16 10:12:31 +0000

    Update Discord.js

--- apps/Discord.js
+++ apps/Discord.js
@@ -22,7 +22,8 @@ const Discord = {
     }).style({
       width: '100%',
       height: '100%',
-      border: 'none'
+      border: 'none',
+      background: 'white'
     }).appendTo(win.content)
   }
 }
