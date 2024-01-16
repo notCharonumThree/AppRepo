ade00d755c1de08b93241fb24a413aa79204f331
ThinLiquid <thnlqd@gmail.com>
2024-01-16 10:05:09 +0000

    Update and rename minecraft.js to Eaglercraft.js

---
+++ apps/Eaglercraft.js
@@ -0,0 +1,30 @@
+const Eaglercraft = {
+  config: {
+    name: 'Eaglercraft',
+    type: 'process',
+    icon: 'https://raw.githubusercontent.com/Flow-Works/AppRepo/main/assets/minecraft.svg',
+    targetVer: '1.0.0-indev.0'
+  },
+  run: async (process) => {
+    const wm = await process.loadLibrary('lib/WindowManager')
+    const xor = await process.loadLibrary('lib/XOR')
+    const HTML = await process.loadLibrary('lib/HTML')
+    
+    const win = wm.createWindow({
+      title: 'Eaglercraft',
+      icon: Discord.config.icon,
+      width: 700,
+      height: 500
+    }, process)
+
+    new HTML('iframe').attr({
+      src: `/service/${xor.encode('https://eaglercraft.com/mc/1.8.8/')}`,
+    }).style({
+      width: '100%',
+      height: '100%',
+      border: 'none'
+    }).appendTo(win.content)
+  }
+}
+
+export default Eaglercraft
