918859e83184e310726cd33bfe21f74e06dafa60
ThinLiquid <thnlqd@gmail.com>
2024-01-16 03:26:07 +0000

    Update discord.js

--- apps/discord.js
+++ apps/discord.js
@@ -1,56 +1,30 @@
-export default class DiscordApp {
-  meta = {
+const Discord = {
+  config: {
     name: 'Discord',
-    description: 'Discord is the easiest way to talk over voice, video, and text. Talk, chat, hang out, and stay close with your friends and communities.',
+    type: 'process',
     icon: 'https://raw.githubusercontent.com/Flow-Works/AppRepo/main/assets/discord.svg',
-    pkg: 'discord.discord',
-    version: '1.0.0'
-  }
-
-  async open () {
-    const win = window.wm.createWindow({
+    targetVer: '1.0.0-indev.0'
+  },
+  run: async (process) => {
+    const wm = await process.loadLibrary('lib/WindowManager')
+    const xor = await process.loadLibrary('lib/XOR')
+    const HTML = await process.loadLibrary('lib/HTML')
+    
+    const win = wm.createWindow({
       title: this.meta.name,
       icon: this.meta.icon,
       width: 700,
       height: 500
-    })
+    }, process)
 
-    const xor = {
-      randomMax: 100,
-      randomMin: -100,
-
-      encode: (str) => {
-        if (!str) return str
-        return encodeURIComponent(
-          str
-            .toString()
-            .split('')
-            .map((char, ind) =>
-              ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
-            )
-            .join('')
-        )
-      },
-      decode: (str) => {
-        if (!str) return str
-        const [input, ...search] = str.split('?')
-
-        return (
-          decodeURIComponent(input)
-            .split('')
-            .map((char, ind) =>
-              ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
-            )
-            .join('') + (search.length ? '?' + search.join('?') : '')
-        )
-      }
-    }
-
-    win.content.style.background = 'var(--base)'
-    win.content.innerHTML = `
-      <iframe src="/service/${xor.encode('https://discord.com/app/')}" style="width: 100%;height: 100%;border: none;"/>
-    `
-
-    return win
+    new HTML('iframe').attrs({
+      src: `/service/${xor.encode('https://discord.com/app/')}`,
+    }).style({
+      width: '100%',
+      height: '100%',
+      border: 'none'
+    }).appendTo(win.content)
   }
 }
+
+export default Discord
