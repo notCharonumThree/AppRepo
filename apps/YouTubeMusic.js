e9383353cd975ee4d18ec335629b4ba0b728b5f4
ThinLiquid <thnlqd@gmail.com>
2024-01-16 10:10:21 +0000

    Update and rename youtube-music.js to YouTubeMusic.js

---
+++ apps/YouTubeMusic.js
@@ -0,0 +1,30 @@
+const YouTube = {
+  config: {
+    name: 'YouTube',
+    type: 'process',
+    icon: 'https://raw.githubusercontent.com/Flow-Works/AppRepo/main/assets/youtube.svg',
+    targetVer: '1.0.0-indev.0'
+  },
+  run: async (process) => {
+    const wm = await process.loadLibrary('lib/WindowManager')
+    const xor = await process.loadLibrary('lib/XOR')
+    const HTML = await process.loadLibrary('lib/HTML')
+    
+    const win = wm.createWindow({
+      title: 'YouTube',
+      icon: YouTube.config.icon,
+      width: 700,
+      height: 500
+    }, process)
+
+    new HTML('iframe').attr({
+      src: `/service/${xor.encode('https://youtube.com/')}`,
+    }).style({
+      width: '100%',
+      height: '100%',
+      border: 'none'
+    }).appendTo(win.content)
+  }
+}
+
+export default YouTube
