bb89a265e581270fc537f744dd06af35b878f0c2
notCharonumThree <162779406+notCharonumThree@users.noreply.github.com>
2024-04-20 10:20:44 -0700

    Update Eaglercraft.js

--- apps/Eaglercraft.js
+++ apps/Eaglercraft.js
@@ -18,7 +18,7 @@ const Eaglercraft = {
     }, process)
 
     new HTML('iframe').attr({
-      src: `/service/${xor.encode('https://eaglercraft.com/mc/1.8.8/')}`,
+      src: `/service/${xor.encode('https://eaglercraft.q13x.com/')}`,
     }).style({
       width: '100%',
       height: '100%',
